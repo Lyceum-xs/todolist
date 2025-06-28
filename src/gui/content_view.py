ec19eecab3b3925cc2bc8b8075c773f44d532ae5
Vito Lonius <1718943644@qq.com>
2025-06-28 15:42:01 +0800

    设置habitclockin打卡实现

--- src/gui/content_view.py
+++ src/gui/content_view.py
@@ -1,4 +1,5 @@
 import calendar
+import collections
 from logging import root
 from re import A
 from tkinter import *
@@ -489,20 +490,65 @@ def Habitclockin(root, max_width, max_height):
     style.configure('Hab.TLabel', font = ('consolas', 10))
 
     root.columnconfigure(0, weight = 1)
+    root.rowconfigure(1, weight = 1)
 
     # calendar frame
     #----------------------- begin -----------------------
     calendar_frame = ttk.Frame(root)
     calendar_frame.grid(row = 0, column = 0, sticky = 'ew')
+    clockin_frame = ttk.Frame(root)
+    clockin_frame.grid(row = 1, column = 0, sticky = 'nsew')
+
+    clockin_frame.columnconfigure(0, weight = 1)
 
     nowtime = get_data.gettime()
     target_year = nowtime['year']
     target_month = nowtime['month']
 
+    target_date = {'year' : nowtime['year'], 'month' : nowtime['month'], 'day' : nowtime['day']}
+
+    def clear_clockin():
+        for widget in clockin_frame.grid_slaves():
+            row = widget.grid_info().get('row', -1)
+            if row is not None:
+                widget.destroy()
+
+    def draw_clockin():
+        clear_clockin()
+        
+        date_label = ttk.Label(clockin_frame, text = f'{target_date['year']}-{target_date['month']}-{target_date['day']}', style = 'Hab.TLabel')
+        date_label.grid(row = 0, column = 0, sticky = 'w', padx = (10, 0))
+        
+        if target_date['year'] == nowtime['year'] and target_date['month'] == nowtime['month'] and target_date['day'] == nowtime['day']:
+            null_label = ttk.Label(clockin_frame)
+            null_label.grid(row = 2, column = 0, sticky = 'ew')
+
+            clockin_content = tk.StringVar()
+            clockin_content.set('Clockin content')
+
+            clockin_bar = tk.Entry(clockin_frame, textvariable = clockin_content, width = 45, font = ('consolas', 12))
+            clockin_bar.grid(row = 1, column = 0, pady = 45)
+
+            def clo():
+                value = clockin_content.get()
+                print(f'{value} is clockined')
+
+            clockin_button = ttk.Button(clockin_frame, text = 'clockin', style = 'Hab.TButton', command = clo)
+            clockin_button.grid(row = 2, column = 1)
+        
+        elif target_date['year'] < nowtime['year'] or target_date['month'] < nowtime['month'] or target_date['day'] < nowtime['day']:
+            content_label = ttk.Label(clockin_frame, text = 'You didn\'t clockin on this day', style = 'Hab.TLabel')
+            content_label.grid(row = 1, column = 0, pady = 50)
+        
+        else:
+            content_label = ttk.Label(clockin_frame, text = 'This day has not arrived', style = 'Hab.TLabel')
+            content_label.grid(row = 1, column = 0, pady = 50)
+
+
     def clear_calendar():
         for widget in calendar_frame.grid_slaves():
             row = widget.grid_info().get('row', -1)
-            if row is not None and row >= 2:
+            if row is not None and row > 1:
                 widget.destroy()
 
     def draw_calendar(calendar):
@@ -511,8 +557,14 @@ def Habitclockin(root, max_width, max_height):
             c = week % 7
             if week == 7:
                 r += 1
-            def game(time):
-                print(f'{time} is now')
+            def game(target_day):
+                print(f'{target_day} is now')
+                nonlocal target_date
+                target_date['year'] = target_year
+                target_date['month'] = target_month
+                target_date['day'] = target_day
+
+                draw_clockin()
             day_button = ttk.Button(calendar_frame, text = day, style = 'Hab.TButton', width = 5, command = lambda d = day: game(d))
             day_button.grid(row = r, column = c, padx = 10, pady = 20)
 
@@ -572,14 +624,21 @@ def Habitclockin(root, max_width, max_height):
     draw_calendar(calendar)
 
     def back_to_today():
-        nonlocal target_year, target_month
+        nonlocal target_year, target_month, target_date
         target_year = nowtime['year']
         target_month = nowtime['month']
 
+        target_date['year'] = nowtime['year']
+        target_date['month'] = nowtime['month']
+        target_date['day'] = nowtime['day']
+
         update_calendar()
+        draw_clockin()
 
     back_to_today_button = ttk.Button(calendar_frame, text = 'back', style = 'Hab.TButton', command = back_to_today)
     back_to_today_button.grid(row = 0, column = 6)
+
+    draw_clockin()
     #------------------------ End ------------------------
 
 
